4121ff5c74e86422b98afc6f564a629a54b7fdd7
Lisa Shaughnessy <lisavshaughnessy@gmail.com>
2018-03-24 22:48:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,5 +57,10 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (!cardNumber) {return "Sorry, we don't have a credit card on file"
+  if (!cardNumber) {
+    return "Sorry, we don't have a credit card on file for you."
+  }
+  var cartTotal = total()
+  
 }
+  

9b1642ceed7ec714ab2d2a2fb382d0e86e0c67cc
Lisa Shaughnessy <lisavshaughnessy@gmail.com>
2018-03-24 19:33:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ function setCart(c) {
 
 function addToCart(item) {
  var price = Math.floor(Math.random() * 100)
-cart.push({ [item]: price })
-console.log(`${item} has been added to your cart.`)
-return cart
+ var item = {(item:price)};
+ cart.push(item);
+ console.log(`${item} has been added to your cart.`);
 }
 
 function viewCart() {

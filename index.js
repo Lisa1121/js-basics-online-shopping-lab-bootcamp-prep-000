455a58156f55c5d0c40c0668764c95f57c98c748
Lisa Shaughnessy <lisavshaughnessy@gmail.com>
2018-03-24 20:41:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,13 +10,18 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  var price = Math.floor(Math.random()*100)
-  cart.push({[item]:price})
-  console.log(`${item} has been added to`)
+  cart.push({[item]:Math.floor(Math.random()*100)})
+  console.log(`${item} has been added to your cart.`)
+  return cart
 }
 
 function viewCart() {
-  
+  if(cart.length===0){
+    console.log("Your shopping cart is empty.")
+  }
+  else if (cart.length===1){
+    
+  }
 }
 function total() {
   // write your code here

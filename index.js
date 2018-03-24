33c8525432fc19469f3d71c0251c3124bf6b0076
Lisa Shaughnessy <lisavshaughnessy@gmail.com>
2018-03-24 22:44:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -44,7 +44,16 @@ function total() {
 }
 
 function removeFromCart(item) {
-  
+  if(cart.length===0) {
+    return "That item is not in your cart."
+  }
+  for (var i=0; i<cart.length; i++) {
+    if(cart[i].itemName===item) {
+      cart.splice(i,1)
+      return cart
+    }
+  }
+  return "That item is not in your cart."
 }
 
 function placeOrder(cardNumber) {

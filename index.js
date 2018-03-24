fe4fc626cfc446d234aad01c3cb5a930964adadb
Lisa Shaughnessy <lisavshaughnessy@gmail.com>
2018-03-24 20:53:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function viewCart() {
     console.log("Your shopping cart is empty.")
   }
   else if (cart.length===1){
-    console.log(`In your cart, you have ${Object.keys(cart[0])} at $${cart)[0][Object.keys(cart[0])]}.`)
+    console.log(`In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}.`)
   }
   else if (cart.length ===2){
     console.log(`In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]} and ${Object.keys(cart[1])} at $${cart[1][Object.keys(cart[1])]}.`)
